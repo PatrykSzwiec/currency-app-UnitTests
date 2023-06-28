714bc5629e918833626d761620a47a3328340c7a
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-06-28 20:39:55 +0200

    Add Unit Tests at utils/__tests__

--- src/utils/__tests__/convertPLNtoUSD.js
+++ src/utils/__tests__/convertPLNtoUSD.js
@@ -1,5 +1,4 @@
-import { convertPLNToUSD } from './../convertPLNToUSD';
-
+import { convertPLNToUSD } from '../convertPLNToUSD';
 
 describe('ConvertPLNtoUSD', () => {
   it('should return proper value when good input', () => {
@@ -8,14 +7,24 @@ describe('ConvertPLNtoUSD', () => {
     expect(convertPLNToUSD(20)).toBe('$5.71');
     expect(convertPLNToUSD(12)).toBe('$3.43');
   });
-
-  it('should return NaN when input is string', () => {
-    expect(isNaN(convertPLNToUSD('6'))).toBe(true);
-    expect(isNaN(convertPLNToUSD('abc'))).toBe(true);
-    expect(isNaN(convertPLNToUSD('-543'))).toBe(true);
+  it('should return NaN when input is text', () => {
+    expect(convertPLNToUSD('2')).toBeNaN();
+    expect(convertPLNToUSD('abc')).toBeNaN();
+    expect(convertPLNToUSD('-543')).toBeNaN();
+  });
+  it('should return NaN when input is empty', () => {
+    expect(convertPLNToUSD()).toBeNaN();
+  });
+  it('should return "Error" when input is different than number and string', () => {
+    expect(convertPLNToUSD({})).toBe('Error');
+    expect(convertPLNToUSD([])).toBe('Error');
+    expect(convertPLNToUSD(null)).toBe('Error');
+    expect(convertPLNToUSD(function() {})).toBe('Error');
   });
 
-  it('should return NaN when input is empty', () => {
-    expect(isNaN(convertPLNToUSD())).toBe(true);
+  it('should return zero when input is lower than zero', () => {
+    expect(convertPLNToUSD(-1)).toBe('$0.00');
+    expect(convertPLNToUSD(-2)).toBe('$0.00');
+    expect(convertPLNToUSD(-56)).toBe('$0.00');
   });
 });
--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,35 @@
 export const convertPLNToUSD = (PLN) => {
 
-  const PLNtoUSD = PLN / 3.5;
-  
+  // Calculate the PLN to USD conversion
+  const PLNtoUSD = parseFloat(PLN) / 3.5;
+
+  // Create a number formatter for USD currency format
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
   });
 
+  // Check if PLN is a string or undefined, return NaN is such cases
+  if (typeof PLN === 'string' || PLN === undefined) {
+    return NaN;
+  }
+
+  // Check if PLN is neither a string nor a number, return 'Error' in such cases
+  if (typeof PLN !== 'string' && typeof PLN !== 'number') {
+    return 'Error';
+  }
+
+  // Check if PLN is less than 0, return '$0.00' in such cases
+  if (PLN < 0) {
+    return '$0.00';
+  }
+
+  // Check if PLN is null, return NaN in such cases
+  if (PLN === null) {
+    return NaN;
+  }
+
+  // Format the PLN to USD conversion using the number formatter
+  // and replace any non-breaking space characters
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
 }
